d016c79e48fc91568babd494297541e22b1460db
ASayid <abdelrahman.sayid@asset.com.eg>
2018-07-18 17:45:52 +0200

    AddLOL

---
+++ spring-web-services-master/spring-web-services-master/ManageSoapPr/src/main/java/com/abdelrahman/ManageSoapPr/ManageSoapPrApplication.java
@@ -0,0 +1,12 @@
+package com.abdelrahman.ManageSoapPr;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ManageSoapPrApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ManageSoapPrApplication.class, args);
+	}
+}
